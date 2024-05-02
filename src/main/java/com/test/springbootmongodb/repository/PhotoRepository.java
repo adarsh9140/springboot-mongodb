ca90b25079c82a444ab9c55ff1d319e5481eafdc
GuptaAdarsh10 <123458574+GuptaAdarsh10@users.noreply.github.com>
2024-05-02 11:30:24 +0530

    first commit

---
+++ src/main/java/com/test/springbootmongodb/repository/PhotoRepository.java
@@ -0,0 +1,9 @@
+package com.test.springbootmongodb.repository;
+
+import com.test.springbootmongodb.collection.Photo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends MongoRepository<Photo, String> {
+}
